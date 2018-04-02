5b0c73f8bf6c36d930db36913f1189deda6eebde
Ryosuke Okuta <okuta@preferred.jp>
2018-04-02 21:46:45 +0900

    Fix test

--- tests/test_daz.py
+++ tests/test_daz.py
@@ -9,28 +9,29 @@ import daz
 class TestDaz(unittest.TestCase):
 
     def setUp(self):
+        self.scale = 128
         self.normal = sys.float_info.min
-        self.denormal = sys.float_info.min / 2
+        self.denormal = sys.float_info.min / self.scale
 
     def check_normal(self):
-        assert self.normal == self.denormal + self.denormal
-        assert self.normal / 2 == self.denormal
+        assert self.normal == self.denormal * self.scale
+        assert self.normal / self.scale == self.denormal
 
     def test_normal(self):
         self.check_normal()
 
     def test_daz(self):
         daz.set_daz()
-        assert self.normal / 2 == 0
-        assert self.denormal * 2 == 0
+        assert self.normal / self.scale == 0
+        assert self.denormal * self.scale == 0
         assert self.denormal == 0
         daz.unset_daz()
         self.check_normal()
 
     def test_ftz(self):
         daz.set_ftz()
-        assert self.normal / 2 == 0
-        assert self.denormal * 2 == self.normal
+        assert self.normal / self.scale == 0
+        assert self.denormal * self.scale == self.normal
         assert self.denormal != 0
         daz.unset_ftz()
         self.check_normal()
@@ -39,28 +40,29 @@ class TestDaz(unittest.TestCase):
 class TestDazWithNumPy(unittest.TestCase):
 
     def setUp(self):
+        self.scale = 128
         self.normal = np.full((3,), sys.float_info.min)
-        self.denormal = np.full((3,), sys.float_info.min) / 2
+        self.denormal = np.full((3,), sys.float_info.min) / self.scale
 
     def check_normal(self):
-        np.testing.assert_equal(self.normal, self.denormal + self.denormal)
-        np.testing.assert_equal(self.normal / 2, self.denormal)
+        np.testing.assert_equal(self.normal, self.denormal * self.scale)
+        np.testing.assert_equal(self.normal / self.scale, self.denormal)
 
     def test_normal(self):
         self.check_normal()
 
     def test_daz(self):
         daz.set_daz()
-        np.testing.assert_equal(self.normal / 2, 0)
-        np.testing.assert_equal(self.denormal * 2, 0)
+        np.testing.assert_equal(self.normal / self.scale, 0)
+        np.testing.assert_equal(self.denormal * self.scale, 0)
         np.testing.assert_equal(self.denormal, 0)
         daz.unset_daz()
         self.check_normal()
 
     def test_ftz(self):
         daz.set_ftz()
-        np.testing.assert_equal(self.normal / 2, 0)
-        np.testing.assert_equal(self.denormal * 2, self.normal)
+        np.testing.assert_equal(self.normal / self.scale, 0)
+        np.testing.assert_equal(self.denormal * self.scale, self.normal)
         assert np.all(self.denormal != 0)
         daz.unset_ftz()
         self.check_normal()
